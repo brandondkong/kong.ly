9ad3a03d424127efa80b08da7dcc243c3414e11e
Brandon <kongbrandon0@gmail.com>
2026-06-25 18:52:14 -0500

    feat(links): add optional umami analytics

--- apps/links.kong.ly/src/env.d.ts
+++ apps/links.kong.ly/src/env.d.ts
@@ -2,6 +2,11 @@
 /// <reference types="@sanity/astro/module" />
 
 declare global {
+    interface ImportMetaEnv {
+        readonly UMAMI_WEBSITE_ID?: string;
+        readonly UMAMI_SRC?: string;
+    }
+
     interface Window {
         __updateCursorSize?: (text: string) => void;
     }
